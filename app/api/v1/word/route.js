ecab4819ad29dc39e0e709c4c114369a22b9f780
Divyesh Parmar <divyeshpar2496@gmail.com>
2023-07-21 21:27:33 +0530

    [add]: create word and push to db

--- app/api/v1/word/route.js
+++ app/api/v1/word/route.js
@@ -1,20 +1,20 @@
 import { connect, disconnect } from '@/app/db/connection';
 import { NextResponse } from 'next/server';
 
-async function POST(req, res) {
+export async function POST(requestBody) {
   try {
     const connection = await connect();
-    const { word, definition } = req.body;
-    const sql = `INSERT INTO words (word, definition) VALUES (?, ?)`;
 
+    const { word, definition } = await requestBody.json();
+    const sql = `INSERT INTO words (word, definition) VALUES (?, ?)`;
     const values = [word, definition];
-    const result = await connection.query(sql, values);
+
+    const [row, fields] = await connection.query(sql, values);
 
     await disconnect(connection);
-    return NextResponse.json({ data: result, ok: true }, { status: 201 });
+
+    return NextResponse.json({ data: { row: row, fields: fields } }, { status: 201 });
   } catch (error) {
-    return NextResponse.json({ ...error, ok: false }, { status: 500 });
+    return NextResponse.json({ error: error }, { status: 500 });
   }
 }
-
-export { POST };
